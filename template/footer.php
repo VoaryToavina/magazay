1cd0fd202565c81466062e72616d0a671d9698fc
VoaryToavina <164861695+VoaryToavina@users.noreply.github.com>
2025-03-09 04:16:04 +0300

    Add files via upload

---
+++ template/footer.php
@@ -0,0 +1,32 @@
+
+<!-- Footer -->
+<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 border-top bg-dark bg-gradient fixed-bottom mt-3">
+      <div class="col-md-2 d-flex align-items-center">
+          <a href="./public/index.php" class="mb-3 ms-3 me-2 mb-md-0 text-white text-decoration-none lh-1" >
+              <i class="bi-bag-fill"></i>
+              
+          </a>
+          <span class="text-white">&copy; 2025 Company, Inc</span>
+      </div>
+  
+      <ul class="nav col-md-4 justify-content-end list-unstyled d-flex me-4">
+        <!-- <span class="text-muted"><i>Contact :</i></span> -->
+          <li class="ms-3" title="Mangataka stage">
+              <a class="text-decoration-none text-white" href="https://www.linkedin.com/in/voary-toavina/">LinkedIn
+                  <i class="fa-linkedin fa-brands"></i>
+              </a>
+          </li>
+          <li class="ms-3" title="Abonnez-vous !">
+            <a class="text-decoration-none text-white" href="https://www.instagram.com/voary.toa/" >instagram
+                  <i class="fa-instagram fa-brands"></i>
+  
+              </a></li>
+          <li class="ms-3" title="Raha misy fanontaniana">
+            <a class="text-decoration-none text-white" href="https://www.facebook.com/voary.ndrina.18">facebook
+                  <i class="fa-facebook fa-brands"></i>
+              </a></li>
+      </ul>
+  </footer>
+<link rel="stylesheet" href="/assets/js/bootstrap.bundle.min.js">
+</body>
+</html>
---
+++ template/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="fontawesome-free-6.5.1-web/css/all.min.css">
+    <link rel="stylesheet" href="../fontawesome-free-6.5.1-web/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <title>MagaZay</title>
+</head>
+
+<body class='' style="background-color: #EFF3EA;">
+    <header class="container-fluid bg-dark p-3 mb-3 ">
+        <nav class="nav-brand text-white ">
+            <a href="./" class="text-decoration-none text-white" title="HOME">
+                <button class="btn btn-outline-warning"><h5><i class="fa-shop fa-solid mt-2"> MagaZay </i></h5></button>
+        </a>
+           
+            </nav>
+    </header>
+    
